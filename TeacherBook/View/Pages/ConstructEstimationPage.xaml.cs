2f2ac597d11ee35ae5ec7a70a19a64fde27b4527
nerooon123 <keleron123@gmail.com>
2023-01-25 14:37:08 +0500

    fc

--- TeacherBook/View/Pages/ConstructEstimationPage.xaml.cs
+++ TeacherBook/View/Pages/ConstructEstimationPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TeacherBook.Model;
 
 namespace TeacherBook.View.Pages
 {
@@ -20,16 +21,51 @@ namespace TeacherBook.View.Pages
     /// </summary>
     public partial class ConstructEstimationPage : Page
     {
+        Core db = new Core();
         public ConstructEstimationPage()
         {
             InitializeComponent();
+            GroupNameComboBox.ItemsSource = db.context.Groups.ToList();
+            GroupNameComboBox.DisplayMemberPath = "NameGroup";
+            GroupDataGrid.ItemsSource = db.context.Students.ToList();
+            GroupDataGrid.ItemsSource = db.context.Groups.ToList();
+            GroupDataGrid.ItemsSource = db.context.Subjects.ToList();
+
         }
 
         private void GroupNameComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            int obj = Convert.ToInt32(GroupNameComboBox.SelectedValue);
+            int obj = 1 + Convert.ToInt32(GroupNameComboBox.SelectedIndex);
 
-            GroupDataGrid.ItemsSource = db.context.Groups.Where(x => x.IdGroup == obj).ToList();
+            GroupDataGrid.ItemsSource = db.context.Journals.Where(x => x.IdGroup == obj).ToList();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                var item = GroupDataGrid.SelectedItem as Journals;
+
+                //проверка того, что пользователь выбрал строки для удаления
+
+                if (item == null)
+
+                {
+                    MessageBox.Show("Вы не выбрали ни одной строки");
+                }
+                else
+                {
+                    db.context.SaveChanges();
+                    MessageBox.Show("Данные успешно добавлениы!", "Увидомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                    this.NavigationService.Navigate(new MenuPage());
+                }
+            }
+            catch (Exception)
+
+            {
+                MessageBox.Show("Данные не отредактированы. ");
+            }
         }
     }
 }
